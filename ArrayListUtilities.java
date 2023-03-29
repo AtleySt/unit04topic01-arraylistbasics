f7c2d7b9a43928f16393a75cd86f2437df5a0d84
AtleySt <atleystvrain@gmail.com>
2023-03-29 12:01:26 -0500

    1, 2, 3, done, unable to test 3

--- ArrayListUtilities.java
+++ ArrayListUtilities.java
@@ -1,7 +1,32 @@
 import java.util.ArrayList;
 
 public class ArrayListUtilities {
+
     public static int getListMax(ArrayList<Integer> items) {
-        return 0;
+        int listMax = items.get(0);
+        for (int i = 1; i < items.size(); i++) {
+            if (items.get(i) > listMax) {
+                listMax = items.get(i);
+            }
+        }
+        return listMax;
+    }
+
+    public static ArrayList<String> withoutLongWords(ArrayList<String> items) {
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).length() <= 5) {
+                result.add(items.get(i));
+            }
+        }
+        return result;
+    }
+
+    public static void removeLongWords(ArrayList<String> items) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).length() > 5) {
+                items.remove(i);
+            }
+        }
     }
 }
--- ArrayListUtilitiesTest.java
+++ ArrayListUtilitiesTest.java
@@ -13,9 +13,33 @@ public class ArrayListUtilitiesTest {
         int actualOutput;
 
         // some positive numbers
-        input = new ArrayList<>(Arrays.asList(1, 5, 4, 9, 2, 1));
-        expectedOutput = 9;
+        input = new ArrayList<>(Arrays.asList(1, 20, 4, 9, 2, 1));
+        expectedOutput = 20;
         actualOutput = ArrayListUtilities.getListMax(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+    @Test
+    public void testWithoutLongWords() {
+        ArrayList<String> input;
+        ArrayList<String> expectedOutput;
+        ArrayList<String> actualOutput;
+
+        input = new ArrayList<String>(Arrays.asList("Cow","AtleyS","Pig","APCompSci","APCSA"));
+        expectedOutput = new ArrayList<String>(Arrays.asList("Cow","Pig","APCSA"));
+        actualOutput = ArrayListUtilities.withoutLongWords(input);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    // come back, not sure how to test void methods
+    @Test
+    public void testRemoveLongWords() {
+        ArrayList<String> input;
+        ArrayList<String> expectedOutput;
+        ArrayList<String> actualOutput;
+
+        input = new ArrayList<String>(Arrays.asList("Cow","AtleyS","Pig","APCompSci","APCSA"));
+        expectedOutput = new ArrayList<String>(Arrays.asList("Cow","Pig","APCSA"));
+        actualOutput = input;
         assertEquals(expectedOutput, actualOutput);
     }
 }
